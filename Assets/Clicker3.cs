d2eb9b8af37a07033a70ee2bc8b3b6f144c574f2
Naomi-Ifergan <55000614+Naomi-Ifergan@users.noreply.github.com>
2021-11-23 09:38:44 -0500

    money upgrade

--- Assets/Clicker3.cs
+++ Assets/Clicker3.cs
@@ -3,43 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Clicker2 : MonoBehaviour
+public class Clicker3 : MonoBehaviour
 {
 
-    public int score = 1000000;
+    public int score = 0;
     public Text scoreText;
-    public int mult = -1;
+    public int mult = 5;
 
     public Button buy2XButton;
 
     public void AddPoints()
     {
-        score -= 1 * mult;
+        score += 5 * mult;
         scoreText.text = "Score: " + score;
 
     }
 
-    public void Buy2xButtonUpgrade2()
+    public void Buy2xButtonUpgrade3()
     {
-        mult = -2;
+        mult = 10;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        buy2XButton.enabled = true;
+        buy2XButton.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (score >= 999990)
+        if (score >= 50)
         {
-            buy2XButton.enabled = false;
+            buy2XButton.enabled = true;
         }
         else
         {
-            buy2XButton.enabled = true;
+            buy2XButton.enabled = false;
         }
     }
 }
